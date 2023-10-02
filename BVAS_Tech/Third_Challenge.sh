5ff940ecfc2335acbe4049265340c46480ca6280
unknown <ibrahimbiliamin02@yahoo.com>
2023-10-02 12:06:06 -0400

    aim

---
+++ BVAS_Tech/Third_Challenge.sh
@@ -0,0 +1,27 @@
+
+
+
+	Next challenge: 
+
+-Create a Github Account
+- create an online repository 
+Create a Readme file 
+-Clone repo on a Terminal (CLI ) 
+-Create a directory (BVAS_Tech) and add a 
+file name (Third_Challenge) content (Copy this 
+third challenge instructions as content of the file) 
+-Create a branch, create directory in the branch and
+ a file add the aim of this group as  content of the file 
+-Merge to the master branch 
+-Push to the online repository
+
+our aim is to connect IT engineers who have taken some sort
+of training or have attended boot camps with the much needed
+work experience.
+We take our students through some rigorous challenges where
+they ramp up expertise as they go through different stages.
+after which winners are rewarded by Inviting them to work
+on real jobs where they will be mentored by senior developers etc
+We set out task to complete within time frame -
+
+
